e025253b7d6cb4f4b62d345c603555e7a4a6787c
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-13 13:44:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(namg, grader id=nil)
+  
+  
+  end
 
 
 end
