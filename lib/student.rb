1e78284fe90fbf946423485e944868f14b9da845
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-13 14:32:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
   
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
+      SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row| 
       self.new_from_db(row)
