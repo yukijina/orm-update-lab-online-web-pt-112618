6eeb9a2f08c35f7604ea10e0ff939ea9762252fa
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-13 14:16:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
       INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, name, grade)
-    @id = DB[:conn].execute("SELECT students last_insert_rowid()")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
 
 
