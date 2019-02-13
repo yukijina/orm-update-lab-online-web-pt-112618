936f2d99a95df2ab0e1b6258129baaa9b1b9b2d8
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-13 14:31:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
       SELECT * FROM students WHERE name = ?
     SQL
     binding.pry
-    DB[:conn].execute(sql, name) map do |row| 
+    DB[:conn].execute(sql, name).map do |row| 
       self.new_from_db(row)
     end 
   end 
