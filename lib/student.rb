a742f2a8e32d0caf843196ae22f1bc757ff8cdaa
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-13 13:46:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,12 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  def initialize(namg, grader id=nil)
-  
-  
+  attr_accessor :name, :grade 
+  attr_reader :id
+  def initialize(id=nil, name, grade)
+    @id = id 
+    @name =  name 
+    @grade = grade
   end
 
 
